f21a9bcb9657ca45077804bfff939ab9f669a129
akbasliogulcan <ogulcanakbsli@gmail.com>
2025-10-28 21:11:13 +0300

    second

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector('#menu-icon');
+const menu = document.querySelector('.navbar');
+
+console.log(menuIcon);
+console.log(menu);
+
+menuIcon.addEventListener("click", () => {
+           menu.classList.toggle('open-menu')
+});
